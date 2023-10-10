60161211b4e5eca4eb5dab9f41c18e2fdc671f6b
Elizaveta <krylovaliza@yandex.ru>
2023-10-10 21:44:46 +0300

    Add app_context to tests

--- ПроКомплаенс/ImportService/tests.py
+++ ПроКомплаенс/ImportService/tests.py
@@ -1,23 +1,28 @@
 from datetime import datetime, timedelta
 import unittest
 from app import app, db
-from app.models import User, Post
+from app.models import User
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
-        db.create_all()
+        with app.app_context():
+            db.create_all()
 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
+            
     def test_password_hashing(self):
-        u = User(username='lisa')
-        u.set_password('win')
-        self.assertFalse(u.check_password('lose'))
-        self.assertTrue(u.check_password('win'))
+        with app.app_context():
+            u = User(username='lisa')
+            u.set_password('win')
+            self.assertFalse(u.check_password('lose'))
+            self.assertTrue(u.check_password('win'))
 
 
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
+    
